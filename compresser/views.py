54efdf6af4293ab717015c0bf4bbb88c99f21b98
Mouiad JRA <mouiad.alilo@gmail.com>
2022-07-25 23:45:19 +0300

    fix: change the output a little

--- compresser/views.py
+++ compresser/views.py
@@ -12,10 +12,8 @@ def upload(request):
         if form.is_valid():
             compresser = form.save()
             compressed_image = compress(compresser.image)
-            print(compressed_image)
-            ctx = {"compressed_image": compressed_image }
+            ctx = {"compressed_image": compressed_image}
             return render(request, 'main/results.html', ctx)
-        return redirect("compressor:upload")
     form = CompressorForm()
     images = Compressor.objects.all()
     return render(request=request, template_name="main/upload.html", context={'form': form, 'images': images})
